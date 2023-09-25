dbaf249226048bb3f5968b9b7a14c3839fd3c644
Balaj27 <93006537+Balaj27@users.noreply.github.com>
2023-09-26 00:36:01 +0500

    first commit

---
+++ src/main/java/com/mycompany/finalproject/FinalProject.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.finalproject;
+
+/**
+ *
+ * @author Hp
+ */
+public class FinalProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
